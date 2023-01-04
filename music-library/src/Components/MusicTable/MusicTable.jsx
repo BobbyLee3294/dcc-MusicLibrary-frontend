7142c9bdd46e8c2bbac3d19fa3b41a9bd98a5f5d
Bobby Lee <thayungbobby@gmail.com>
2023-01-03 20:37:20 -0500

    corrected table layout

--- music-library/src/Components/MusicTable/MusicTable.jsx
+++ music-library/src/Components/MusicTable/MusicTable.jsx
@@ -1,32 +1,28 @@
-import React, { useEffect, useState } from "react";
-
 const MusicTable = (props) => {
-  const [TableData, setTableData] = useState([]);
-  useEffect(() => {
-    let tempTableData = props.parentSongs.map((song) => {
-      return [
-        song.title,
-        song.artist,
-        song.album,
-        song.release_date,
-        song.genre,
-        song.likes,
-      ];
-    });
-    setTableData(tempTableData);
-  }, [props.parentSongs]);
   return (
     <div>
       <h1>Songs</h1>
-      {TableData.map(function (song, index) {
+      {props.parentSongs.map(function (song, index) {
         return (
-          <div key={index}>
-            <p>{song.title}</p>
-            <p>{song.artist}</p>
-            <p>{song.album}</p>
-            <p>{song.release_date}</p>
-            <p>{song.genre}</p>
-            <p>{song.likes}</p>
+          <div>
+            <table>
+              <tr>
+                <th>Title</th>
+                <th>Artist</th>
+                <th>Album</th>
+                <th>Release Date</th>
+                <th>Genre</th>
+                <th>Likes</th>
+              </tr>
+              <tr key={index}>
+                <td>{song.title}</td>
+                <td>{song.artist}</td>
+                <td>{song.album}</td>
+                <td>{song.release_date}</td>
+                <td>{song.genre}</td>
+                <td>{song.likes}</td>
+              </tr>
+            </table>
           </div>
         );
       })}
